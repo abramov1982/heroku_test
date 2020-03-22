f971270962b1cef15b30f5646a4eb06ca6f23efc
Abramov Ilya <abramovio@mail.ru>
2020-03-22 14:38:52 +0300

    Added files and changes required for deployment to heroku

--- my_site/settings.py
+++ my_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
